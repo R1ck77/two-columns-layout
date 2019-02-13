592ea466788eeb6cece11cf7e7c8e78fb3f2efa6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-02-13 10:16:22 +0100

    Add basic demo window

--- src/couchgames/launcher.clj
+++ src/couchgames/launcher.clj
@@ -1,10 +1,23 @@
 (ns couchgames.launcher
   (:require [couchgames.layout])
-  (:import [couchgames.layout TwoColumns])
+  (:import [couchgames.layout TwoColumns]
+           [java.awt Dimension Color]
+           [javax.swing JFrame JPanel]
+           )
   (:gen-class))
+
+(def window-width 800)
+(def window-height 600)
+
+(defn show-in-window [component]
+  (doto (JFrame.)
+    (.setSize (Dimension. window-width window-height))
+    (.setContentPane component)
+    (.setVisible true)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (TwoColumns/create))
+  (show-in-window (doto (JPanel.)
+                    (.setBackground Color/RED)))
 )
--- src/couchgames/layout.clj
+++ src/couchgames/layout.clj
@@ -1,18 +1,19 @@
 (ns couchgames.layout
-  (:import [java.awt LayoutManager])
+  (:import [java.awt LayoutManager Component])
   (:gen-class
    :name couchgames.layout.TwoColumns
    :methods [^:static [create [] java.awt.LayoutManager]]))
 
-(defn -create []
-  (reify LayoutManager
-    (addLayoutComponent [this name comp]
-      )
-    (layoutContainer [this parent]
-      )
-    (minimumLayoutSize [this parent]
-      )
-    (preferredLayoutSize [this parent]
-      )
-    (removeLayoutComponent [this comp]
-      )))
+(defn -create [^Component parent]
+  (let [components (atom [])]
+    (reify LayoutManager
+      (addLayoutComponent [this name comp]
+        (swap! components #(conj % (list name comp))))
+      (layoutContainer [this parent]
+        )
+      (minimumLayoutSize [this parent]
+        )
+      (preferredLayoutSize [this parent]
+        )
+      (removeLayoutComponent [this comp]
+        ))))
