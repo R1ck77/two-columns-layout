d9b3d4510433d746e0454af2ed9b2a75fc24b3f7
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-02-15 18:31:54 +0100

    WIP for better demo content

--- src/couchgames/launcher.clj
+++ src/couchgames/launcher.clj
@@ -23,13 +23,31 @@
   (doto (JTextArea. text)
     (.setBackground color)))
 
+(defn new-dimension [width height]
+  (Dimension. width height))
+
+(defn create-top-left-panel []
+  (doto (new-multi-line-label "This is a text" (Color. 180 255 255))
+    (.setMinimumSize (new-dimension 100 100))))
+
+(defn create-bottom-left-panel []
+  (new-panel (Color. 200 255 255)))
+
+(defn create-top-right-panel []
+  (new-multi-line-label "This is a\nmulti line\ntext" (Color. 255 180 255)))
+
+(defn create-bottom-right-panel []
+  (new-panel (Color. 255 200 255)))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [content (JPanel.)
         layout (layout/create content
-                              (new-multi-line-label "This is a text" (Color. 180 255 255)) (new-panel (Color. 200 255 255))
-                              (new-multi-line-label "This is a\nmulti line\ntext" (Color. 255 180 255)) (new-panel (Color. 255 200 255)))]
+                              (create-top-left-panel) (create-bottom-left-panel)
+                              (create-top-right-panel) (create-bottom-right-panel)                              
+                              )]
     (.setLayout content layout)
 ;;;    (.add content (new-panel (Color. 180 255 255)) "head")
 ;;;    (.add content (new-panel (Color. 200 255 255)) "body")
